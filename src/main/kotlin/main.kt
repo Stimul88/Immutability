b7b63fa4ce2424b67f44ea346b2be3194a77ac5c
Stimul88 <ivanov2288@gmail.com>
2025-11-12 18:48:22 +0300

    Fix program

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,12 +1,10 @@
 fun main() {
-
     println("Иванов Евгений")
     commission()
 
     people()
 
     musicLover()
-
 }
 
 
@@ -15,7 +13,7 @@ fun commission() {
     val minCommission = 35
     val transfer  = amount * 0.0075
 
-    val totalCommission = if (transfer < 35)  minCommission else transfer
+    val totalCommission = if (transfer < minCommission)  minCommission else transfer
 
     println(totalCommission)
 }
@@ -25,22 +23,19 @@ fun people() {
 
     val people = if (likes % 100 in 11..14) "людям" else if (likes % 10 == 1) "человеку" else "людям"
 
-
     println("Понравилось $likes $people")
 }
 
 fun musicLover() {
     val regularCustomer = true
-    val sum = 15000
+    val sum = 100.0
 
-    val firstSum = if (sum <= 0 && sum <= 1000) 0 else if (sum <= 1001 && sum <= 10_000) 100 else Math.round(sum * 0.95)
-    val secondDiscount = if (regularCustomer) firstSum * (0.99) else 0
+    val firstSum = if (sum <= 0 && sum <= 1000) sum - 100 else if (sum > 10_000) sum * 0.95 else sum
+    println(firstSum)
 
     if (regularCustomer) {
-        println("Итоговая стоимость:  $secondDiscount")
+        println("Итоговая стоимость:  ${firstSum * 0.99}")
     } else {
         println("Итоговая стоимость:  $firstSum")
     }
-
-
 }
